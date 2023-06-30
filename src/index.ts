db1052f38da5c3e5df2e29de816082e596d6ef59
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2023-06-30 18:22:03 +0900

    横画面にしてみた

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ const main = async () => {
     canvas.height = window.innerHeight;
     ctx?.fillStyle = "red";
     ctx?.fillRect(0, 0, canvas.width, canvas.height);
+    ctx?.fillStyle = "black";
+    ctx?.fillText("aiueo", 50, 50);
 
     if (canvas.requestFullscreen) {
         await canvas.requestFullscreen();
+        if (screen.orientation.lock) {
+            screen.orientation.lock('landscape');
+        }
     }
 }
 
